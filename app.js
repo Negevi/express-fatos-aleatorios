eb8314c5b1af765a04d703b58f1abfce60b66d94
Negevi <lukenicklemke@gmail.com>
2024-03-10 20:37:09 -0300

    commit2

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Set the directory for static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, "js")));
-// Set up Pug as the view engine
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
@@ -17,15 +16,16 @@ app.get('/login', (req, res) => {
     res.sendFile('main.html', { root: path.join(__dirname, "views") });
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, "views") })
-})
 app.post('/login', (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
 
-// Start the server
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, "views") })
+})
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 }).on('error', (e) => {
